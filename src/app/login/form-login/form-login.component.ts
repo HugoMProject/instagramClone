d0239f9d4dbd9f6838bee05ab34d4dc421b59b83
Hugo moyano <hugomoyano1996@gmail.com>
2023-05-13 15:47:49 -0300

    se finalizo la vista de log in del clone de instagram, falta la version movil

--- src/app/login/form-login/form-login.component.ts
+++ src/app/login/form-login/form-login.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.css']
 })
-export class FormLoginComponent {
+export class FormLoginComponent implements OnInit{
+
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+  AlertInstagram(){
+    window.alert('Atencion!!.....Este sitio es un clone de Instagram, con fines didacticos. por ende no debe agragar niguna de sus cuentas, correo o claves, Disculpe las molestas ocacionadas.....');
+
+  }
 
 }
